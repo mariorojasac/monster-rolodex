fc1bd63161562c5c77913d94ba86023a1a019e88
mariorojasac <77466868+mariorojasac@users.noreply.github.com>
2022-04-06 15:51:46 -0400

    adding set state

--- src/App.js
+++ src/App.js
@@ -4,22 +4,26 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() { 
+    super();
+    this.state = {
+       name: 'Mario',
+    }
+  }
+
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>Hi {this.state.name}</p>
+          <button onClick={() => {
+            this.setState({ name: 'Mason', })
+            console.log(this.state)
+          }}
+          >Change Name</button>
         </header>
       </div>
     );
