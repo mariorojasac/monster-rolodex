f8385a39243ae543cb125e1205ecd7ce5cdaabf1
mariorojasac <77466868+mariorojasac@users.noreply.github.com>
2022-04-12 19:28:16 -0400

    async setstate

--- src/App.js
+++ src/App.js
@@ -4,26 +4,38 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
-  constructor() { 
+  constructor() {
     super();
     this.state = {
-       name: 'Mario',
-    }
+      name: "Mario",
+      company: "Moser",
+    };
   }
-
-
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Hi {this.state.name}</p>
-          <button onClick={() => {
-            this.setState({ name: 'Mason', })
-            console.log(this.state)
-          }}
-          >Change Name</button>
+          <p>
+            Hi {this.state.name}, I work at {this.state.company}
+          </p>
+          <button
+            //correct way of setting state 
+            onClick={() => {
+              this.setState(
+                () => {
+                  return { name: "Mason", company: "Apple " };
+                },
+                //after state is up to date is when next call back will run
+                () => {
+                  console.log(this.state);
+                }
+              );
+            }}
+          >
+            Change Name
+          </button>
         </header>
       </div>
     );
